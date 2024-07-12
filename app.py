715d98b4deda25ec1f06bf928a1c27d938c8e19f
Vishak Kashyap K <143735113+vishakkashyapk30@users.noreply.github.com>
2024-07-12 22:44:10 +0530

    added project files

---
+++ app.py
@@ -0,0 +1,5 @@
+from y2k_editor import app
+
+if __name__ == '__main__':
+    # app.run(port=8000)
+    app.run(debug=True, port=8000)
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get -y install ffmpeg
+pip install -r requirements.txt
